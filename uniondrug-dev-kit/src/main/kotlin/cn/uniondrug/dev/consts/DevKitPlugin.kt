4513869d30ec07728e89231a2fdeb10167f48119
dingshichen <foreverhuiqiao@126.com>
2022-06-28 18:17:00 +0800

    Java 插件首次安装或更新的弹窗

--- uniondrug-dev-kit/src/main/kotlin/cn/uniondrug/dev/consts/DevKitPlugin.kt
+++ uniondrug-dev-kit/src/main/kotlin/cn/uniondrug/dev/consts/DevKitPlugin.kt
@@ -2,10 +2,13 @@
 package cn.uniondrug.dev.consts
 
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.util.IconLoader
 
 object DevKitPlugin {
 
     const val NAME = "Uniondrug Dev Kit"
+
+    const val ID = "cn.uniondrug.uniondrug-golang-kit"
 
 }
 
@@ -16,5 +19,11 @@ object DevIcons {
 
     @JvmField
     val DOC_VIEW = AllIcons.CodeWithMe.CwmPermissionView
+
+    @JvmField
+    val UNIONDRUG_LIGHT = IconLoader.getIcon("META-INF/pluginIcon.svg", DevIcons::class.java)
+
+    @JvmField
+    val UNIONDRUG_DARK = IconLoader.getIcon("META-INF/pluginIcon_dark.svg", DevIcons::class.java)
 
 }
--- uniondrug-dev-kit/src/main/kotlin/cn/uniondrug/dev/notifier/CommonNotifier.kt
+++ uniondrug-dev-kit/src/main/kotlin/cn/uniondrug/dev/notifier/CommonNotifier.kt
@@ -3,9 +3,11 @@
  */
 package cn.uniondrug.dev.notifier
 
+import com.intellij.notification.BrowseNotificationAction
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
+import javax.swing.Icon
 
 private val NOTIFIER = NotificationGroupManager.getInstance()
     .getNotificationGroup("Uniondrug.Notification.Group")
@@ -18,3 +20,10 @@ fun notifyWarn(project: Project, content: String) =
 
 fun notifyError(project: Project, content: String) =
     NOTIFIER.createNotification(content, NotificationType.ERROR).notify(project)
+
+fun notifyStartup(project: Project, icon: Icon) {
+    NOTIFIER.createNotification("药联 Java 开发者工具", "使用方法在 Wiki <br> 如果遇到麻烦、或者有什么需求和建议，可以在群里联系开发者～～", NotificationType.INFORMATION)
+        .setIcon(icon)
+        .addAction(BrowseNotificationAction("Wiki", "http://wiki.turboradio.cn/pages/viewpage.action?pageId=32968218"))
+        .notify(project)
+}
